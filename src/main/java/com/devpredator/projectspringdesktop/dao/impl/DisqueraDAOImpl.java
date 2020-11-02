3fbf97f1a1c4227f55af74058b5a563a97c9a595
di3gokanon89 <diego.paniagua.kanon89@gmail.com>
2020-11-02 09:55:30 -0600

    Nombre del Proyecto: project-spring-desktop Descripcion: Se generan beans en el archivo applicationContext.xml y se cargan las propiedades para obtener datos desde el archivo config.properties.

---
+++ src/main/java/com/devpredator/projectspringdesktop/dao/impl/DisqueraDAOImpl.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package com.devpredator.projectspringdesktop.dao.impl;
+
+import org.springframework.stereotype.Repository;
+
+import com.devpredator.projectspringdesktop.dao.DisqueraDAO;
+
+/**
+ * @author DevPredator
+ * Clase que implementa la funcionalidad del CRUD de disqueraDAO y con la cual
+ * crearemos el BEAN con SPRING.
+ */
+@Repository
+public class DisqueraDAOImpl implements DisqueraDAO {
+
+}
